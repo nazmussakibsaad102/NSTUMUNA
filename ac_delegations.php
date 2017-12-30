d90159c6b8d8380a7d465ec5f6cf5bef453b1f34
sakibsaad102@gmail.com <sakibsaad102@gmail.com>
2017-12-31 03:52:09 +0600

    first commit

---
+++ ac_delegations.php
@@ -0,0 +1,381 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<!-- Meta, title, CSS, favicons, etc. -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>NSTUMUNA </title>
+	<link rel="shortcut icon" href="assets/icons/logo.png">
+	
+	<!-- Styles -->
+	<link rel='stylesheet' href='assets/css/bootstrap.min.css'>
+	<link rel='stylesheet' href='assets/css/animate.min.css'>
+	<link rel='stylesheet' href="assets/css/font-awesome.min.css"/>
+	<link rel='stylesheet' href="assets/css/style.css"/>
+	
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Raleway:200,300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		  <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+			
+	<!-- Favicon -->
+	<link rel="shortcut icon" href="#">
+</head>
+<body>
+<!-- Begin Hero Bg -->
+<div id="parallax">
+</div>
+<!-- End Hero Bg
+	================================================== -->
+<!-- Start Header
+	================================================== -->
+<header id="header" class="navbar navbar-inverse navbar-fixed-top" role="banner">
+<div class="container">
+<div class="navbar-header">
+  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+  <span class="sr-only">Toggle navigation</span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  <span class="icon-bar"></span>
+  </button>
+  <!-- Your Logo -->
+  <a href="index.php" class="navbar-brand"> <span><img src="assets/icons/logoBig.png" alt=""></span> NSTUMUNA <span class="lighter"></span></a>
+</div>
+<!-- Start Navigation -->
+<nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
+<ul class="nav navbar-nav">
+  <li>
+  <a href="index.php#home">Home</a>
+  </li>
+  <li>
+  <a href="index.php#about">About</a>
+  </li>
+  <li>
+  <a href="index.php#administration">Administration</a>
+  </li>
+  <li>
+  <a href="index.php#gallery_front">Gallery</a>
+  </li>
+  <li>
+  <a href="index.php#membership">Membership</a>
+  </li>
+  <li>
+  <a href="index.php#events">Events</a>
+  </li>
+  <li>
+  <a href="index.php#academics">Academics</a>
+  </li>
+  <li>
+  <a href="index.php#news">News</a>
+  </li>
+  <li>
+  <a href="index.php#contact_us">Contact Us</a>
+  </li>
+  <li>
+  <a href="mun18.php">NSTUMUN 2018</a>
+  </li>
+  
+</ul>
+</nav>
+</div>
+</header>
+
+
+
+
+<!-- Gallery
+	================================================== -->
+<section class="parallax section" style="background-image: url(http://themepush.com/demo/runcharity/assets/img/2.jpg);">
+    <div class="parallax-overlay" style="background-color:#00c1c1;opacity:0.9;"></div>
+    <div class="wrapsection">
+	<div class="parallax-overlay" style="background-color:#00c1c1;opacity:0.9;"></div>
+	<div class="container">
+			<div class="col-md-12 sol-sm-12">
+					<div class="maintitle">
+							<h3 class="section-title">Delegations-Awards-EB Members</h3>	
+              </div>
+              <div class="table-responsive">
+							<table class="table  table-striped black table table-bordered" style="text-align:center">
+									<thead >
+									  <tr>
+										<th>SL</th>
+										<th>Conference</th>
+										<th>Delegates</th>
+									
+										<th>Awards</th>
+										<th>Executive board Members</th>
+										
+									  
+									  </tr>
+									</thead>
+									<tbody>
+											
+										
+                                    <tr>
+                                    <td>01</td>
+                                    <td> DUNMUN 2013  </td>
+                                    <td>5 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>02</td>
+                                    <td> CUMUN 2015 </td>
+                                    <td> 3</td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>03</td>
+                                    <td> JMUN 2015 </td>
+                                    <td>6 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>04</td>
+                                    <td> BIMUN 2015 </td>
+                                    <td>2 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>05</td>
+                                    <td> DUNMUN 2015 </td>
+                                    <td>1 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>06</td>
+                                    <td> CUMUN 2016</td>
+                                    <td>26 </td>
+                                    <td> 1 Best Delegate, 1 Outstanding Delegate </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>07</td>
+                                    <td> RUMUN 2016 </td>
+                                    <td> 10</td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>08</td>
+                                    <td> BUPMUN 2016 </td>
+                                    <td>8 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>09</td>
+                                    <td> UNYSABMUN 2016</td>
+                                    <td> 39</td>
+                                    <td>1 Special Mention, 2 Position Papers </td>
+                                    <td> 1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td> JMUN 2016  </td>
+                                    <td> 12</td>
+                                    <td>1 Best Delegate, 2 Special Mentions, 1 Position Paper </td>
+                                    <td>1 Deputy Speaker </td>
+                                  </tr>
+                                <tr>
+                                    <td>11</td>
+                                    <td> NSUIMUN 2016 </td>
+                                    <td>1 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>12</td>
+                                    <td> CSMUN 2016 </td>
+                                    <td>2 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>13</td>
+                                    <td> CUMUN 2017 </td>
+                                    <td>3 </td>
+                                    <td> </td>
+                                    <td>1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>14</td>
+                                    <td> RUMUN 2017 </td>
+                                    <td>10 </td>
+                                    <td>2 Position Papers </td>
+                                    <td> 1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>15</td>
+                                    <td> Diplomatic Assembly
+                  Rangpur </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td>1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>16</td>
+                                    <td> LUMUN 2017 </td>
+                                    <td>3 </td>
+                                    <td> 1 Position Paper </td>
+                                    <td>1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>17</td>
+                                    <td> Diplomatic Assembly
+                  Dhaka </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> 1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>18</td>
+                                    <td> NSTUMUN 2017 </td>
+                                    <td>59 </td>
+                                    <td>1 Best Delegate, 
+                  1 Special Mention, 
+                  1 Position Paper </td>
+                                    <td>1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>19</td>
+                                    <td> SDGMUN Global </td>
+                                    <td> 1</td>
+                                    <td>1 Outstanding Delegate </td>
+                                    <td> 1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>20</td>
+                                    <td> TMSSCR Intra MUN 2017 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td>1 Chairperson, 
+                  1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>21</td>
+                                    <td> JMUN 2017 </td>
+                                    <td>10 </td>
+                                    <td> 1 Special Mention </td>
+                                    <td>1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>22</td>
+                                    <td> CCPCMUN 2017 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td> 1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>23</td>
+                                    <td> BANMUN 2017 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td>1 Co-Chairperson, 
+                  1 Vice Chairperson </td>
+                                  </tr>
+                                <tr>
+                                    <td>24</td>
+                                    <td> Sylhet Regional MUN Summit 2017 </td>
+                                    <td> 4</td>
+                                    <td>1 Best Delegate, 
+                  2 Outstanding Delegate </td>
+                                    <td>1 Co-Chairperson, 
+                  1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>25</td>
+                                    <td> BUPMUN 2017 </td>
+                                    <td>8 </td>
+                                    <td> 1 Special Mention </td>
+                                    <td> </td>
+                                  </tr>
+                                <tr>
+                                    <td>26</td>
+                                    <td> UNYSABMUN 2017 </td>
+                                    <td>8 </td>
+                                    <td>1 Outstanding Delegate, 
+                  1 Special, 
+                  1 Position Paper </td>
+                                    <td> 1 Chairperson, 
+                  1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>27</td>
+                                    <td> NSUIMUN 2017 </td>
+                                    <td>1 </td>
+                                    <td> </td>
+                                    <td>1 Director </td>
+                                  </tr>
+                                <tr>
+                                    <td>28</td>
+                                    <td> DUNMUN 2017 </td>
+                                    <td>2 </td>
+                                    <td> 1 Special Mention </td>
+                                    <td>1 Chairperson, 
+                  1 Director </td>
+                                  </tr>
+                                  <tr>
+                                    <td>29</td>
+                                    <td> TMSMUN 2018 </td>
+                                    <td> </td>
+                                    <td> </td>
+                                    <td>1 Director </td>
+                                  </tr>
+                  
+                  
+                  
+                                  
+
+
+									
+						  
+								  
+								  
+									</tbody>
+              </table>
+            </div>
+				    
+			</div>
+</section>
+
+
+
+<!-- Credits 
+=============================================== -->
+<section id="credits" class="text-center" style="background-color:#00c1c1;opacity:0.9;">
+<span class="social wow zoomIn">
+<a href="https://www.facebook.com/nstumuna/"><i class="fa fa-facebook"></i></a>
+
+<a href="https://www.instagram.com/nstumuna/"><i class="fa fa-instagram"></i></a>
+
+
+<a href="https://www.youtube.com/channel/UC_E8pNF5R0XDIWl8XRCOlYQ"><i class="fa fa-youtube-play"></i></a>
+</span><br/>
+Copyright &copy; <a href="#">NSTUMUNA</a> 
+
+</section>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/waypoints.min.js"></script>
+<script src="assets/js/jquery.scrollTo.min.js"></script>
+<script src="assets/js/jquery.localScroll.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/validate.js"></script>
+<script src="assets/js/common.js"></script>
+<script src="assets/js/jquery.samesize.js"></script>
+</body>
+</html>
